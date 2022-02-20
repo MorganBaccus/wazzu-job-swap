fea8cc89179324d6b677fb86b64173cfebc815c4
Morgan <morganbaccus@live.com>
2022-02-20 01:00:42 -0800

    Message icon shows in the top right corner and all header images are displayed correctly.

--- wazzu-job-swap/src/components/Header.js
+++ wazzu-job-swap/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './../App.css';
 import './Header.css';
 import PersonIcon from '@mui/icons-material/Person';
-//import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 import { IconButton } from '@mui/material';
 
 function Header() {
@@ -14,6 +14,7 @@ function Header() {
                 src="https://1000logos.net/wp-content/uploads/2021/07/Washington-State-Cougars-logo.png"
                 alt="logo"
             />
+            <ChatBubbleIcon class="header__icon" fontSize="large"/>
         </div>
     );
 }
