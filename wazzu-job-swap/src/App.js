4a61e5e2620832338a45e66164f64e4499c9d1d4
Morgan <morganbaccus@live.com>
2022-02-19 23:22:14 -0800

    Images are shown on the page.

--- wazzu-job-swap/src/App.js
+++ wazzu-job-swap/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Switch, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './components/Home'
 import About from './components/About' 
+import Header from './components/Header' 
 
 function App() {
   return (
     <div>
-      <p>hullo</p>
+      <Header/>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
--- wazzu-job-swap/src/components/Home.js
+++ wazzu-job-swap/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './../App.css';
 
+
 function Home() {
     return (
         <div>
